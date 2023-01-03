53247df599a3eef755b81a795d7b2a4a1aa96369
Alif Wibowo <alif.likes.coding@gmail.com>
2023-01-03 17:15:54 +0700

    Add function logic

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,17 @@
 def bubble_sort(array)
-    
+    not_sorted = false
+    for i in 0..array.length-1 do
+      array.each_with_index do |elm, idx|
+        if (idx < array.length - 1) then
+            if (array[idx] > array[idx + 1]) then
+                temp = array[idx]
+                array[idx] = array[idx+1]
+                array[idx+1] = temp
+            end
+        end
+      end
+    end
+    array
 end
 
-bubble_sort([4,3,78,2,0,2])
+p bubble_sort([4,3,78,2,0,2])
